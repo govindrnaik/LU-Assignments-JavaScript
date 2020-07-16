f1beaa0b96968bee10001b561c7252a2e3edd7bb
govindrnaik <57467998+govindrnaik@users.noreply.github.com>
2020-07-16 15:19:11 +0530

    Add files via upload

---
+++ DAY 2/script/stringsandarrays.js
@@ -0,0 +1,16 @@
+let strg = "i am govind and i am learning javascript";
+
+console.log(strg);
+console.log(strg.bold());
+console.log(strg.indexOf("j"));
+console.log(strg.trim());
+console.log(strg.fontcolor("red"));
+console.log(strg.replace("govind","tushar"));
+console.log(strg.sup());
+console.log(strg.strike());
+
+let arr = [23,45,45,54, "govind",545,5454,556,4,5];
+console.log(arr);
+console.log(arr.indexOf(45));
+console.log(arr.unshift(23));
+console.log(arr.values());
