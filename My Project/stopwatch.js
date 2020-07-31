e6d75a2206cbc80b778f4f34968ee503d33ddc38
govindrnaik <57467998+govindrnaik@users.noreply.github.com>
2020-07-31 18:57:48 +0530

    Add files via upload

---
+++ My Project/stopwatch.js
@@ -0,0 +1,24 @@
+let d = prompt("ef","feb 8, 2021")
+var countDownDate = new Date(`${d} 00:00:00`).getTime();
+
+console.log();
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("stopwatch").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("stopwatch").innerHTML = "TIMES UP";
+  }
+}, 1000);
